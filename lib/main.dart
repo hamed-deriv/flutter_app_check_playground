f97ba7eef034f7097af056e963f319f48c28c736
hamed-fs <hamed@firstsource.io>
2021-11-23 08:04:47 +0330

    add app check

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,26 +36,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final Future<FirebaseApp> firebaseApp = Firebase.initializeApp();
+  int? value;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Flutter Demo Home Page')),
       body: Center(
-        child: FutureBuilder(
-          future: firebaseApp,
+        child: FutureBuilder<void>(
+          future: _initializeAppCheck(),
           builder: (context, snapshot) {
             if (snapshot.hasError) {
               return Text(snapshot.error.toString());
             } else if (snapshot.connectionState == ConnectionState.done) {
-              return const Text('Success!');
+              return Text(
+                '${value ?? "NO VALUE"}',
+                style:
+                    const TextStyle(fontSize: 75, fontWeight: FontWeight.bold),
+              );
             } else {
               return const CircularProgressIndicator();
             }
           },
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          value = _insertSampleDataIntoFirebaseDatabase();
+
+          setState(() {});
+        },
+        child: const Icon(Icons.refresh),
+      ),
     );
+  }
+
+  Future<void> _initializeAppCheck({bool enableAppCheck = false}) async {
+    await Firebase.initializeApp();
+
+    if (enableAppCheck) {
+      await FirebaseAppCheck.instance.activate();
+    }
+  }
+
+  int _insertSampleDataIntoFirebaseDatabase() {
+    final int randomNumber = Random().nextInt(100);
+    final DatabaseReference databaseReference =
+        FirebaseDatabase.instance.reference().child('test');
+
+    databaseReference.set({
+      'value': randomNumber,
+    });
+
+    return randomNumber;
   }
 }
